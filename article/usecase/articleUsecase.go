aa345236cdad1c69640d9c103d8d2fd36f0c6f70
rufa-a <roofinda@gmail.com>
2020-11-12 15:05:38 +0300

    added test file and go:generate for mocks

--- article/usecase/articleUsecase.go
+++ article/usecase/articleUsecase.go
@@ -1,5 +1,7 @@
 package usecase
 
+//go:generate mockgen -destination=../mocks/mock_ArticleUsecase.go -package=mocks github.com/go-park-mail-ru/2020_2_AVM/article ArticleUsecase
+
 import (
 	"github.com/go-park-mail-ru/2020_2_AVM/article"
 	"github.com/go-park-mail-ru/2020_2_AVM/models"
---
+++ article/usecase/articleUsecase_test.go
@@ -0,0 +1,37 @@
+package usecase_test
+
+import "testing"
+
+func TestDeleteArticle(t *testing.T) {
+}
+func TestGetCategoryID(t *testing.T) {
+}
+func TestGetTagID(t *testing.T) {
+}
+
+func TestGetArticlesByName(t *testing.T) {
+}
+func TestGetArticlesByAuthorId(t *testing.T) {
+}
+
+func TestGetArticlesByCategory(t *testing.T) {
+}
+
+func TestGetArticlesByTag(t *testing.T) {
+
+}
+func TestGetArticlesByTags(t *testing.T) {
+
+}
+
+func TestGetArticlesBySubscribe(t *testing.T) {
+
+}
+
+func  TestGetArticleIdByNameAndAuthorId(t *testing.T) {
+
+}
+
+func TestLinkTagAndArticle(t *testing.T) {
+
+}
